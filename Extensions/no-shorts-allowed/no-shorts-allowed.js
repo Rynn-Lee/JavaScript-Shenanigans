0a1e756fd1a50ae16678ef4a460b29e5e84a390b
Rynn Lee <20amigo04@gmail.com>
2024-01-16 23:40:15 +0600

    Remove "Shorts" buttons

--- Extensions/no-shorts-allowed/no-shorts-allowed.js
+++ Extensions/no-shorts-allowed/no-shorts-allowed.js
@@ -1,22 +1,58 @@
-var shortsShelves = document.getElementsByTagName("ytd-reel-shelf-renderer")
-var shortsShelvesRich = document.getElementsByTagName("ytd-rich-shelf-renderer")
 var countrycode = document.getElementById("country-code")
-
 countrycode.innerHTML = "Everrynn✨"
+let removedButtons = false
+let removedShorts = false
 
-const obliterateShorts = (shorts) => {
-  for(let i = 0; i < shorts.length; i++){
-    let shelve = shorts[i]
-    shelve.innerHTML = ""
-    shelve.style.display = "none"
+
+const obliterateButtons = () => {
+  const PCButtons = document.getElementsByClassName("yt-simple-endpoint")
+  const MiniButtons = document.getElementsByTagName("ytd-mini-guide-entry-renderer")
+
+  if((!PCButtons.length || !MiniButtons.length) && !removedButtons){
+    setTimeout(()=>{
+      obliterateButtons()
+      console.log("AGAIN BUTTONS", removedButtons)
+    }, 100)
+    return
+  }
+  removedButtons = true
+  for(let i = 0; i < PCButtons.length; i++){
+    let button = PCButtons[i]
+    if(button.getAttribute('title') == "Shorts"){button.remove()}
+  }
+  
+  for(let i = 0; i < MiniButtons.length; i++){
+    let button = MiniButtons[i]
+    if(button.getAttribute('aria-label') == "Shorts"){button.remove()}
+  }
+}
+const obliterateShorts = () => {
+  var shortsShelves = document.getElementsByTagName("ytd-reel-shelf-renderer")
+  var shortsShelvesRich = document.getElementsByTagName("ytd-rich-shelf-renderer")
+  if((!shortsShelves.length && !shortsShelvesRich.length) && !removedShorts){
+    setTimeout(()=>{
+      obliterateShorts()
+    }, 100)
+    return
+  }
+  removedShorts = true
+  for(let i = 0; i < shortsShelves.length; i++){
+    let shelve = shortsShelves[i]
+    shelve.remove()
+  }
+  for(let i = 0; i < shortsShelvesRich.length; i++){
+    let shelve = shortsShelvesRich[i]
+    shelve.remove()
   }
 }
 
 window.onload = function(){
-  try{
-    obliterateShorts(shortsShelves)
-    obliterateShorts(shortsShelvesRich)
-  } catch (error) {
-    alert("ERROR:", error)
-  }
+    obliterateShorts()
+    obliterateButtons()
 }
+
+window.addEventListener('popstate', function (event) {
+    obliterateShorts()
+    alert("ERROR:", error)
+});
+

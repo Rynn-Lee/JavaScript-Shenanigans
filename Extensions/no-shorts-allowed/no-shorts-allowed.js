1900e79bb1f4e225da8c565ef94f2b0ef77f4b7e
Rynn Lee <20amigo04@gmail.com>
2024-01-17 20:50:52 +0600

    Rewrote (still unstable)

--- Extensions/no-shorts-allowed/no-shorts-allowed.js
+++ Extensions/no-shorts-allowed/no-shorts-allowed.js
@@ -1,41 +1,21 @@
-var countrycode = document.getElementById("country-code")
-countrycode.innerHTML = "Everrynn✨"
-let removedButtons = false
-let removedShorts = false
-
-
 const obliterateButtons = () => {
   const PCButtons = document.getElementsByClassName("yt-simple-endpoint")
   const MiniButtons = document.getElementsByTagName("ytd-mini-guide-entry-renderer")
-
-  if((!PCButtons.length || !MiniButtons.length) && !removedButtons){
-    setTimeout(()=>{
-      obliterateButtons()
-      console.log("AGAIN BUTTONS", removedButtons)
-    }, 100)
-    return
-  }
-  removedButtons = true
+  if(!PCButtons.length && !MiniButtons.length){return}
   for(let i = 0; i < PCButtons.length; i++){
     let button = PCButtons[i]
     if(button.getAttribute('title') == "Shorts"){button.remove()}
   }
-  
   for(let i = 0; i < MiniButtons.length; i++){
     let button = MiniButtons[i]
     if(button.getAttribute('aria-label') == "Shorts"){button.remove()}
   }
+  setTimeout(obliterateButtons, 100)
 }
 const obliterateShorts = () => {
   var shortsShelves = document.getElementsByTagName("ytd-reel-shelf-renderer")
   var shortsShelvesRich = document.getElementsByTagName("ytd-rich-shelf-renderer")
-  if((!shortsShelves.length && !shortsShelvesRich.length) && !removedShorts){
-    setTimeout(()=>{
-      obliterateShorts()
-    }, 100)
-    return
-  }
-  removedShorts = true
+  if(!shortsShelvesRich.length && !shortsShelves.length){return}
   for(let i = 0; i < shortsShelves.length; i++){
     let shelve = shortsShelves[i]
     shelve.remove()
@@ -44,15 +24,44 @@ const obliterateShorts = () => {
     let shelve = shortsShelvesRich[i]
     shelve.remove()
   }
+  setTimeout(obliterateShorts, 100)
+}
+
+const changeCountry = () => {
+  let countrycode = document.getElementById("country-code")
+  if(countrycode.innerHTML == "Everrynn✨"){return}
+  countrycode.innerHTML = "Everrynn✨"
+  changeCountry()
+}
+
+const checkIfShortsPage = () => {
+  let pathArray = window.location.pathname.split('/');
+  if(!pathArray.includes("shorts")){return}
+  window.location.replace("https://leetcode.com/problemset/");
+} 
+
+const resized = () => {
+  changeCountry()
+  obliterateShorts()
+  obliterateButtons()
+  checkIfShortsPage()
 }
 
 window.onload = function(){
-    obliterateShorts()
-    obliterateButtons()
+  changeCountry()
+  obliterateShorts()
+  obliterateButtons()
+  checkIfShortsPage()
 }
 
 window.addEventListener('popstate', function (event) {
-    obliterateShorts()
-    alert("ERROR:", error)
-});
+  changeCountry()
+  obliterateShorts()
+  checkIfShortsPage()
+}, {passive: true});
 
+let resizeTimer;
+window.onresize = function(){
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(resized, 100);
+};
